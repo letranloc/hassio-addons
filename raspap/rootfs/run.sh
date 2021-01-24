d2ef77dec823049e103c1585b3b8fee7bfc7c795
letranloc <abc@letranloc.com>
2021-01-24 13:45:32 +0700

    up/down wlan0 interface

--- raspap/rootfs/run.sh
+++ raspap/rootfs/run.sh
@@ -121,6 +121,8 @@ if [ ${#HOSTAPD_CONFIG_OVERRIDE} -ge 1 ]; then
   done
 fi
 
+ip link set $INTERFACE down
+
 iptables-nft -t nat -C POSTROUTING -o eth0 -j MASQUERADE || iptables-nft -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 iptables-nft -C FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT || iptables-nft -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACCEPT
 iptables-nft -C FORWARD -i wlan0 -o eth0 -j ACCEPT || iptables-nft -A FORWARD -i wlan0 -o eth0 -j ACCEPT
@@ -130,6 +132,8 @@ iptables -A FORWARD -i eth0 -o wlan0 -m state --state RELATED,ESTABLISHED -j ACC
 iptables -A FORWARD -i wlan0 -o eth0 -j ACCEPT
 
 iptables-save | tee /etc/iptables/rules.v4
+
+ip link set $INTERFACE up
 
 # Setup dnsmasq.conf if DHCP is enabled in config
 if [ $DHCP -eq 1 ]; then
